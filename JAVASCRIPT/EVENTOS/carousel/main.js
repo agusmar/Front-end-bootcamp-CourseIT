bff057e573ae5ca7a815103f269b54086bb2f765
Agus Mar <44821883+Agustinchen@users.noreply.github.com>
2019-06-19 17:33:11 -0300

    update main.js

--- JAVASCRIPT/EVENTOS/carousel/main.js
+++ JAVASCRIPT/EVENTOS/carousel/main.js
@@ -1,2 +1,7 @@
 
-document.getElementByClass(
+function press() {
+// const parrafo = document.getElementById("parrafo");
+// parrafo.innerHTML = document.getElementById("inputValue").value;
+
+document.getElementById("parrafo").innerHTML = document.getElementById("inputValue").value;
+}
