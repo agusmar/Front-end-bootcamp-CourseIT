827123bcc67d48417978f20359ea2c5c6cf641b7
Agus Mar <44821883+Agustinchen@users.noreply.github.com>
2019-06-19 17:05:41 -0300

    JAVASCRIPT

---
+++ JAVASCRIPT/EVENTOS/main.js
@@ -0,0 +1,7 @@
+
+function press() {
+// const parrafo = document.getElementById("parrafo");
+// parrafo.innerHTML = document.getElementById("inputValue").value;
+
+document.getElementById("parrafo").innerHTML = document.getElementById("inputValue").value;
+}
