d9df84c0c66481f561dd8819cd7732516b367e66
Agus Mar <44821883+Agustinchen@users.noreply.github.com>
2019-06-19 21:17:59 -0300

    update main.js

--- JAVASCRIPT/EVENTOS/carousel/main.js
+++ JAVASCRIPT/EVENTOS/carousel/main.js
@@ -1,2 +1,33 @@
+const items = document.getElementsByClassName("item");
+const itemsLength = items.length;
+const slides = document.getElementById("slides-container");
+let izq = 0;
 
-document.getElementByClass(
+for (let i = 0; i < itemsLength; i++) {
+  createListItem(i + 1);
+}
+
+function createListItem(val) {
+  const listItem = document.createElement("li");
+  const text = document.createTextNode(val);
+  listItem.appendChild(text)
+  slides.appendChild(listItem);
+}
+
+function forward() {
+
+  const container = document.getElementById("slider");
+  izq = izq - 50;
+  container.style.left = `${izq}px`;
+}
+
+function backward() {
+  const container = document.getElementById("slider");
+  izq = izq + 50;
+  container.style.left = `${izq}px`;
+}
+
+function goToSlide(number) {
+  const slider = document.getElementById("slider");
+  slider.style.left = (number -1) * 200 * -1 + "px";
+}
